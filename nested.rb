04d2d0947196f44707e20c60ee9627560078a0e6
Irene Saez <bluemilk44@gmail.com>
2019-01-31 23:39:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages][1]
+programmer_hash[:dennis_ritchie][:languages] << Assemby
 
 end
