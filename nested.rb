7486942ecf41702902edf0ceae6899f5b0173f29
Irene Saez <bluemilk44@gmail.com>
2019-01-31 23:38:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     alan_change = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alan_change
+     
+     programmer_hash
      
      
 end
